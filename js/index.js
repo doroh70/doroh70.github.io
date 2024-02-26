5607c8ff0ed88076c9ef8724a203f0bc51a81795
doroh70 <oghenedavid123@gmail.com>
2024-02-26 03:20:48 -0700

    include typewriter effects

--- js/index.js
+++ js/index.js
@@ -15,4 +15,45 @@ document.addEventListener('DOMContentLoaded', function () {
         });
         bsCollapseAbout.hide();
     });
+
+    var typedAbout = document.getElementById('typedAbout');
+
+    var typewriter = new Typewriter(typedAbout, {
+    loop: false,
+    delay: 100,
+    cursor: '✍🏿',
+    });
+
+    
+    typewriter
+    .pasteString('<h1><strong><span style="color: #0288D1;">Hi</span> 👋🏿, </strong></h1>')
+    .pauseFor(300)
+    .typeString('<h2 id="emojiHeader"><span style="color: #212121;">I\'m Oghenemaro</span></h2>')
+    .callFunction(function(state) {
+        // Directly append the emojis to the last typed element (the <h2> in this case)
+        var emojis = ['🫦','🧜🏿‍♂️', '😬', '👨🏿‍💻', '👨🏿‍💼'];
+        var currentEmojiIndex = 0;
+
+        function typeAndDeleteEmoji() {
+            if (currentEmojiIndex < emojis.length) {
+                var emoji = emojis[currentEmojiIndex];
+                var container = document.getElementById('emojiHeader');
+                container.innerHTML += emoji;
+                currentEmojiIndex++;
+                
+                if (currentEmojiIndex != emojis.length){
+                    // Wait some time before deleting the emoji
+                    setTimeout(() => {
+                        container.innerHTML = container.innerHTML.replace(emoji, '');
+                        typeAndDeleteEmoji(); // Type and delete the next emoji
+                    }, 1000); 
+                }      
+            }
+        }
+
+        typeAndDeleteEmoji();
+    })
+    .pauseFor(300 + 1000 * 4)
+    .typeString('Soon to be graduate looking for SDE roles.')
+    .start();
 });
